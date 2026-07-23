2e9552a6489ea7dbe9eec90e163ab0f09f33816c
Fernan7922 <ferg7922@gmail.com>
2026-07-22 23:12:50 -0600

    agregar s2

--- Lab2/lab2_24734/lab2_24734/main.c
+++ Lab2/lab2_24734/lab2_24734/main.c
@@ -12,9 +12,9 @@ int main(void)
 	LCD_Init();
 
 	// ---- Pantalla de bienvenida, se muestra una sola vez al arrancar ----
-	LCD_SetCursor(0, 0);
+	LCD_SetCursor(0, 5);
 	LCD_Print("Lab 2");
-	LCD_SetCursor(1, 0);
+	LCD_SetCursor(1, 1);
 	LCD_Print("ADC y Contador");
 	_delay_ms(2000);     // la dejamos un par de segundos en pantalla
 	LCD_Clear();          // limpiamos antes de arrancar con las lecturas
@@ -31,13 +31,27 @@ int main(void)
 		uint8_t decimal= voltios_x100 %100;
 		
 
-		sprintf(buffer, "%d.%02d V", entero, decimal);  // lo dejamos tal cual, sin convertir a voltaje
+		sprintf(buffer, "%d.%02dV", entero, decimal);  // lo dejamos tal cual, sin convertir a voltaje
 
-		LCD_SetCursor(0, 0);
+		LCD_SetCursor(0, 1);
 		LCD_Print("S1: ");
 		LCD_SetCursor(1, 0);
 		LCD_Print(buffer);
 		LCD_Print("     ");   // espacios de sobra: 1023 tiene mas digitos que 0
+		//Acá se usa la misma lógica para el S2
+		uint16_t lectura2= ADC_Read(1);
+		uint16_t voltios2_x100 = (( uint32_t) lectura2 *500)/1023;
+		uint8_t entero2= voltios2_x100 /100;
+		uint8_t decimal2= voltios2_x100 %100;
+		
+
+		sprintf(buffer, "%d.%02dV", entero2, decimal2);  // lo dejamos tal cual, sin convertir a voltaje
+
+		LCD_SetCursor(0, 7);
+		LCD_Print("S2: ");
+		LCD_SetCursor(1, 6);
+		LCD_Print(buffer);
+		LCD_Print("     ");   // espacios de sobra: 1023 tiene mas
 
 		_delay_ms(200);      // pausa corta para que el valor no ande parpadeando sin parar
 	}
